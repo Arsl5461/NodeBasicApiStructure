09a1683e589095bbbc47f2dee0f6acf8e38c9519
Arslan Akmal <Arslan.akmal@arhamsoft.org>
2023-08-17 21:55:11 +0500

    Port Updated

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const app = express();
-const PORT = 8084;
+const PORT = 8000;
 // const connectDb = require("./config");
 const registerRouter = require("./routes/user.route");
 
 // connectDb();
 app.use(registerRouter);
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-  })
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running ${PORT}`);
