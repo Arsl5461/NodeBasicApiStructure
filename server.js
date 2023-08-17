9921411eb9ea394b53cab96d7d78559c6a477ed6
Arslan Akmal <Arslan.akmal@arhamsoft.org>
2023-08-17 16:21:27 +0500

    Third Commit

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-const express=require("express");
-const app=express();
-const PORT=8084;
-const connectDb=require("./config")
-const registerRouter=require("./routes/user.route")
+const express = require("express");
+const app = express();
+const PORT = 8084;
+const connectDb = require("./config");
+const registerRouter = require("./routes/user.route");
 
 connectDb();
-app.use(registerRouter)
+app.use(registerRouter);
 
+res.json("Hello World!");
 
-
-app.listen(PORT,()=>{
-    console.log(`Server is running ${PORT}`);
-})
+app.listen(PORT, () => {
+  console.log(`Server is running ${PORT}`);
+});
