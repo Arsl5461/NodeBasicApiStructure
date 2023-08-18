b75bb0530da0da8cd164c8d2f6bde322c147fbb4
Arslan Akmal <Arslan.akmal@arhamsoft.org>
2023-08-18 15:31:13 +0500

    New Route Added

--- server.js
+++ server.js
@@ -13,6 +13,9 @@ app.get("/", (req, res) => {
 app.get("/hello", (req, res) => {
   res.send("Hello From Docker Side!");
 });
+app.get("/arsl", (req, res) => {
+  res.send("Hello From Arslan Side!");
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running ${PORT}`);
